7fd0a95dbee5991f11e9cd9d4da78769365ea2b5
Assadi <assadi.halifa@gmail.com>
2021-08-22 16:53:37 +0200

    Update users.route.js

--- server/routes/users.route.js
+++ server/routes/users.route.js
@@ -7,6 +7,8 @@ router.get("/", auth, UserController.getAllUser);
 router.get("/:id", auth, UserController.getOneUser);
 router.put("/:id", auth, UserController.editUser);
 router.patch("/password/:id", auth, UserController.editPassUser);
-router.get("/follower/:id", auth, UserController.getOneUser);
+router.patch("/follower/add/:id", auth, (req, res) => {
+  res.send("test");
+});
 
 module.exports = router;
