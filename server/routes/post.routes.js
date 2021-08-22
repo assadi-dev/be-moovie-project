6012301dbe407d5f6e273fc277668a04ecb10848
Assadi <assadi.halifa@gmail.com>
2021-08-22 02:03:53 +0200

    Update post.routes.js

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -6,5 +6,8 @@ const postController = require("../controllers/post.controller");
 router.get("/", auth, postController.readAllPost);
 router.get("/:id", auth, postController.readOnePost);
 router.post("/add", auth, postController.createPost);
+router.patch("/comment/add", auth, postController.createPost);
+//router.patch("/comment/edit", auth, postController.createPost);
+//router.patch("/comment/edit", auth, postController.createPost);
 
 module.exports = router;
