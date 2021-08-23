a70852ace454a01c3a4c8735b9c3a2e988d1a413
Assadi <assadi.halifa@gmail.com>
2021-08-23 08:02:04 +0200

    Update postComment.controller.js

--- server/controllers/postComment.controller.js
+++ server/controllers/postComment.controller.js
@@ -1,11 +1,46 @@
 const postModel = require("../models/post.model");
 const userModel = require("../models/user.model");
 const userServices = require("../services/user.services");
-const ObjectID = require("mongoose").Types.ObjectId;
+const ent = require("ent");
+const { isValidObjectId } = require("mongoose");
+const dayjs = require("dayjs");
+var utc = require("dayjs/plugin/utc");
+var timezone = require("dayjs/plugin/timezone"); // dependent on utc plugin
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
-exports.addComment = (req, res) => {
+exports.addComment = async (req, res) => {
+  const { id } = req.params;
+  const author = req.body.author;
+  const pseudo = ent.encode(req.body.pseudo);
+  const text = ent.encode(req.body.text);
+  const createdAt = dayjs();
+
   try {
-    res.send("Add comment");
+    if (!isValidObjectId(id)) {
+      throw "Id Invalid";
+    }
+
+    await postModel.findByIdAndUpdate(
+      id,
+      {
+        $push: {
+          comments: {
+            author: author,
+            pseudo: pseudo,
+            text: text,
+            createdAt: createdAt,
+          },
+        },
+      },
+      { new: true },
+      (err, docs) => {
+        if (err) {
+          throw err;
+        }
+        res.status(200).json(docs);
+      }
+    );
   } catch (error) {
     res.status(500).json(error);
   }
