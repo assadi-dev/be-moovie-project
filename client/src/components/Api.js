f74f9686e33fd9ffef0806d20f926ba8ca39863b
Assadi <assadi.halifa@gmail.com>
2021-08-24 01:21:17 +0200

    update

--- client/src/components/Api.js
+++ client/src/components/Api.js
@@ -1,13 +1,14 @@
 import axios from "axios";
-import { useAuthState } from "../utils/context/AuthContext";
 import Cookies from "js-cookie";
 
 const StorageKey = "@MyAppOAuthKey";
 const auth = Cookies.get(StorageKey);
 const token = JSON.parse(auth).token;
 
+console.log(process.env.REACT_APP__URL);
+
 export const api = axios.create({
-  baseURL: process.env.APP_REACT_URL,
+  baseURL: "http://localhost:6500/api",
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${token}`,
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,15 @@
 import { logout } from "../utils/context/AuthAction";
 import { useAuthDispatch, useAuthState } from "../utils/context/AuthContext";
+import { api } from "../components/Api";
 
 const Home = (props) => {
   const userData = useAuthState();
-  const dispatch = useAuthDispatch(); // read dispatch method from context
-
+  const dispatch = useAuthDispatch();
   const handleLogout = () => {
-    logout(dispatch); //call the logout action
-
-    props.history.push("/login"); //navigate to logout page on logout
+    logout(dispatch);
+    props.history.push("/login");
   };
+
   return (
     <div>
       <h1>Home Page</h1>
