ea84cf30bdc836260b30731e9a14a91408d4d93a
Assadi <assadi.halifa@gmail.com>
2021-09-30 20:10:21 +0200

    Update db_handler.js

--- server/db/db_handler.js
+++ server/db/db_handler.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const { MongoMemoryServer } = require("mongodb-memory-server");
 
 exports.dbConnect = async () => {
-  const uri = `mongodb://localhost:27017/test`;
-  //const mongoServer = await MongoMemoryServer.create();
+  //const uri = `mongodb://localhost:27017/test`;
+  const mongoServer = await MongoMemoryServer.create();
 
-  //const uri = await mongoServer.getUri();
+  const uri = await mongoServer.getUri();
 
   console.log(uri);
 
